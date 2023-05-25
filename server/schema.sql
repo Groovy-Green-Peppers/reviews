b49d4edd190d60102fd962c496159299e46eba99
Kodi <30986906+kodiwalker@users.noreply.github.com>
2023-05-25 11:53:21 -0600

    Imported csv's into db

--- server/schema.sql
+++ server/schema.sql
@@ -7,8 +7,8 @@ Create Table If Not Exists Reviews (
   body Varchar(1000),
   recommend boolean,
   reported boolean,
-  reviewer_name Varchar(20),
-  reviewer_email Varchar(30),
+  reviewer_name Varchar(100),
+  reviewer_email Varchar(100),
   response Varchar(1000),
   helpfulness int
 );
@@ -16,7 +16,7 @@ Create Table If Not Exists Reviews (
 Create Table If Not Exists Review_Photos (
   id Serial Primary Key,
   review_id int References Reviews(id),
-  url Varchar(1000),
+  url Varchar(1000)
 );
 
 Create Table If Not Exists Review_Characteristics (
@@ -25,3 +25,7 @@ Create Table If Not Exists Review_Characteristics (
   review_id int References Reviews(id),
   value int
 );
+
+\copy reviews TO 'hR/reviews/server/reviews.csv' DELIMITER ',' CSV HEADER;
+\copy review_photos TO 'hR/reviews/server/reviews_photos.csv' DELIMITER ',' CSV HEADER;
+\copy review_characteristics TO 'hR/reviews/server/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
